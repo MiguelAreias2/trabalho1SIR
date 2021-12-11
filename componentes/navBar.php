d185ddb8f5959f89e22ad557d7d8765c767d82d8
MiguelAreias2 <79462661+MiguelAreias2@users.noreply.github.com>
2021-12-11 16:42:12 +0000

    .

--- componentes/navBar.php
+++ componentes/navBar.php
@@ -1,44 +1,20 @@
+<?php
+require_once './componentes/head.php';
+?>
+
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">HOME</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Apontamentos
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Perfil
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-
-                    </ul>
-                    <form class="d-flex">
-                        <button class="btn btn-outline-primary" type="submit">Logout</button>
-                    </form>
-                </div>
+        <nav>
+            <div class="topnav">
+                <a class="active" href="#home">Home</a>
+                <a href="#news">News</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+                <input type="button" value="Logout">
             </div>
+            
+                
+            
         </nav>
+
+    </header>
--- login.php
+++ login.php
@@ -1,32 +1,32 @@
 <?php
-require_once './componentes/header.php';
+require_once './componentes/head.php';
 require_once './componentes/navBar.php';
 ?>
 
 <div class="container">
     <div class="row">
-        <div class="col-3"></div>
-        <div class="col-6">
-            <div class="text-center">
-                <img src="./assets/logo.png" class="rounded m-3" alt="" width="250" height="250">
+        <div class="col"></div>
+        <div class="col">
+            <div class>
+                <img src="./assets/logo.png" alt="" width="250" height="250">
             </div>
 
-            <div class="rounded m-3 bg-light">
+            <div>
                 <form action="">
-                    <h3 class="text-center p-3">Login</h3>
-                    <div class="input-group flex-nowrap p-3">
-                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+                    <h3>Login</h3>
+                    <div>
+                        <input type="text" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
                     </div>
-                    <div class="input-group flex-nowrap p-3">
-                        <input type="text" class="form-control" placeholder="Password" aria-label="password" aria-describedby="addon-wrapping">
+                    <div>
+                        <input type="text" placeholder="Password" aria-label="password" aria-describedby="addon-wrapping">
                     </div>
-                    <div class="input-group flex-nowrap p-3">
-                        <button type="submit" class="btn btn-outline-primary">Login</button>
+                    <div>
+                        <button type="submit">Login</button>
                     </div>
                 </form>
             </div>
         </div>
-        <div class="col-3"></div>
+        <div class="col"></div>
     </div>
 </div>
 
