b7a81b764db7f5495e0aa4afacba3dfc5ee82a1a
MiguelAreias2 <79462661+MiguelAreias2@users.noreply.github.com>
2021-12-12 01:30:36 +0000

    pagina Login responsiva

--- login.php
+++ login.php
@@ -1,11 +1,12 @@
 <?php
 require_once './database/conexao.php';
-require_once './componentes/head.php';
-require_once './componentes/navBar.php';
+require_once './head.php';
+require_once './navBar.php';
 
 $email = '';
 $passe = '';
 $erro;
+$sessaoIniciada='';
 
 session_start();
 
@@ -23,54 +24,57 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!$user) {
         $erro = 'Dados de login incorretos!';
     }
+    else
+    {
+        $sessaoIniciada = 'Sessão Iniciada!';
+    }
 }
 ?>
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-1"></div>
+            <div class="col-md-6 col-10">
+                <div class="logo">
+                    <img src="./assets/logo.png" alt="" width="250" height="250">
+                </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col"></div>
-        <div class="col">
-            <div class>
-                <img src="./assets/logo.png" alt="" width="250" height="250">
-            </div>
-
-            <div>
-                <form action="">
-                    <h3>Login</h3>
-                    <div>
-                        <input type="text" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
-                    </div>
-                    <div>
-                        <input type="text" placeholder="Password" aria-label="password" aria-describedby="addon-wrapping">
-                    </div>
-
-                    <div class="rounded m-3 bg-light">
+                <div>
+                    <div class="formulario">
                         <form action="" method="POST">
-                            <h3 class="text-center p-3">Login</h3>
-                            <?php if (!empty($erro)) : ?>
-                                <div class="alert alert-danger">
-                                    <div><?php echo $erro ?></div>
+                            <h2 class="">Login</h2>
+                            <?php if (!empty($erro)) { ?>
+                                <div class="">
+                                    <p><?php echo $erro ?></p>
                                 </div>
-                            <?php endif; ?>
-                            <div class="input-group flex-nowrap p-3">
-                                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping" name="email">
-                            </div>
-                            <div class="input-group flex-nowrap p-3">
-                                <input type="text" class="form-control" placeholder="Password" aria-label="password" aria-describedby="addon-wrapping" name="passe">
+                            <?php } else { ?>
+                                <div class="">
+                                    <p><?php echo $sessaoIniciada ?></p>
+                                </div>                            
+                            <?php } ?>
 
+                            <div class="">
+                                <i class="iconEmail"></i>
+                                <input type="text" class="" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping" name="email">
+                            </div>
+                            <div class="">
+                                <input type="text" class="" placeholder="Password" aria-label="password" aria-describedby="addon-wrapping" name="passe">
                             </div>
                             <div>
-                                <button type="submit">Login</button>
+                                <input type="submit" value="Login">
                             </div>
                         </form>
                     </div>
+                </div>
+                <div class="col-md-3 col-1"></div>
             </div>
-            <div class="col"></div>
         </div>
     </div>
+</main>
 
 
-    </header>
-    </body>
 
-    </html>
+</header>
+</body>
+
+</html>
---
+++ navBar.php
@@ -0,0 +1,14 @@
+<body>
+    <header>
+        <nav>
+            <div class="topnav">
+                <a class="active" href="#home">Home</a>
+                <a href="#">Apontamentos</a>
+                <a href="#">Perfil</a>
+                <a href="#" id="botaoLogout">Logout</a>
+                <a href="" class="icon" onclick="">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </div>
+        </nav>
+    </header>
